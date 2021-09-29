90b59648ae4065942590ab51cdad5a0b533aac3d
Thibaut <tgudin@besport.com>
2021-09-29 13:11:16 +0200

    2.1 version : adding set function for config parameters

--- src/hls.mli
+++ src/hls.mli
@@ -157,10 +157,10 @@ val config :
   ?cap_level_to_player_size:bool ->
   ?default_audio_codec:audiocodec ->
   ?initial_live_manifest_size:int ->
-  ?max_buffed_length:int ->
+  ?max_buffer_length:int ->
   ?max_max_buffer_length:int ->
   ?back_buffer_length:int (*defalut value = Infinity*) ->
-  ?max_buffer_size:int * int * int (*TODO*) ->
+  ?max_buffer_size:int ->
   ?max_buffer_hole:float ->
   ?high_buffer_watchdog_period:int ->
   ?nudget_off_set:float ->
@@ -227,7 +227,164 @@ val config :
   config
   [@@js.builder] [@@js.verbatim_names]
 
+val set_auto_start_load : config -> bool -> unit [@@js.set "autoStartLoad"]
+
+val set_start_position : config -> int -> unit [@@js.set "startPosition"]
+
+val set_debug : config -> bool -> unit [@@js.set]
+
+val set_cap_level_on_fps_drop : config -> bool -> unit
+  [@@js.set "capLevelOnFPSDrop"]
+
+val set_cap_level_to_player_size : config -> bool -> unit
+  [@@js.set "capLevelToPlayerSize"]
+
+val set_default_audio_codec : config -> audiocodec -> unit
+  [@@js.set "defaultAudioCodec"]
+
+val set_initial_live_manifest_size : config -> int -> unit
+  [@@js.set "initialLiveManifestSize"]
+
+val set_max_buffer_length : config -> int -> unit [@@js.set "maxBufferLength"]
+
+val set_max_max_buffer_length : config -> int -> unit
+  [@@js.set "maxMaxBufferLength"]
+
+val set_back_buffer_length : config -> int -> unit [@@js.set "backBufferLength"]
+
+val set_max_buffer_size : config -> int -> unit [@@js.set "maxBufferSize"]
+
+val set_max_buffer_hole : config -> float -> unit [@@js.set "maxBufferHole"]
+
+val set_high_buffer_watchdog_period : config -> int -> unit
+  [@@js.set "highBufferWatchdogPeriod"]
+
+val set_nudget_off_set : config -> float -> unit [@@js.set]
+
+val set_nudget_max_retry : config -> int -> unit [@@js.set]
+
+val set_max_frag_look_up_tolerance : config -> float -> unit [@@js.set]
+
+val set_live_sync_duration_count : config -> int -> unit [@@js.set]
+
+val set_live_max_latency_duration_count : config -> int -> unit [@@js.set]
+
+val set_live_duration_infinity : config -> bool -> unit [@@js.set]
+
+val set_enable_worker : config -> bool -> unit [@@js.set]
+
+val set_enable_software_aes : config -> bool -> unit [@@js.set]
+
+val set_manifest_loading_time_out : config -> int -> unit [@@js.set]
+
+val set_manifest_loading_max_retry : config -> int -> unit [@@js.set]
+
+val set_manifest_loading_retry_delay : config -> int -> unit [@@js.set]
+
+val set_manifest_loading_max_retry_timeout : config -> int -> unit [@@js.set]
+
+val set_start_level : config -> int -> unit [@@js.set]
+
+val set_level_loading_time_out : config -> int -> unit [@@js.set]
+
+val set_level_loading_max_retry : config -> int -> unit [@@js.set]
+
+val set_level_loading_retry_delay : config -> int -> unit [@@js.set]
+
+val set_level_loading_max_retry_timeout : config -> int -> unit [@@js.set]
+
+val set_frag_loading_time_out : config -> int -> unit [@@js.set]
+
+val set_frag_loading_max_retry : config -> int -> unit [@@js.set]
+
+val set_frag_loading_retry_delay : config -> int -> unit [@@js.set]
+
+val set_frag_loading_max_retry_timeout : config -> int -> unit [@@js.set]
+
+val set_start_frag_prefetch : config -> bool -> unit [@@js.set]
+
+val set_test_bandwidth : config -> bool -> unit [@@js.set]
+
+val set_progressive : config -> bool -> unit [@@js.set]
+
+val set_low_latency_mode : config -> bool -> unit [@@js.set]
+
+val set_fps_dropped_monitoring_period : config -> int -> unit [@@js.set]
+
+val set_fps_dropped_monitoring_threshold : config -> float -> unit [@@js.set]
+
+val set_append_error_max_retry : config -> int -> unit [@@js.set]
+
+val set_loader : config -> CustomLoader.t -> unit [@@js.set]
+
+val set_f_loader : config -> CustomFragmentLoader.t -> unit [@@js.set]
+
+val set_p_loader : config -> CustomPlaylistLoader.t -> unit [@@js.set]
+
+val set_xhr_setup : config -> XhrSetup.t -> unit [@@js.set]
+
+val set_fetch_setup : config -> FetchSetupCB.t -> unit [@@js.set]
+
+val set_abr_controller : config -> AbrController.t -> unit [@@js.set]
+
+val set_buffer_controller : config -> BufferController.t -> unit [@@js.set]
+
+val set_cap_level_controller : config -> CapLevelController.t -> unit [@@js.set]
+
+val set_fps_controller : config -> FPSController.t -> unit [@@js.set]
+
+val set_timeline_controller : config -> TimelineController.t -> unit [@@js.set]
+
+val set_enable_web_vtt : config -> bool -> unit [@@js.set]
+
+val set_enable_imsc1 : config -> bool -> unit [@@js.set]
+
+val set_enable_cea708_captations : config -> bool -> unit [@@js.set]
+
+val set_stretch_short_video_track : config -> bool -> unit [@@js.set]
+
+val set_max_audio_frames_drift : config -> int -> unit [@@js.set]
+
+val set_forced_key_frame_on_discontinuity : config -> bool -> unit [@@js.set]
+
+val set_abr_ewma_fast_live : config -> float -> unit [@@js.set]
+
+val set_abr_ewma_slow_live : config -> float -> unit [@@js.set]
+
+val set_abr_ewma_fast_vod : config -> float -> unit [@@js.set]
+
+val set_abr_ewma_slow_vod : config -> float -> unit [@@js.set]
+
+val set_abr_ewma_default_estimate : config -> int -> unit [@@js.set]
+
+val set_abr_band_width_factor : config -> float -> unit [@@js.set]
+
+val set_abr_band_width_up_factor : config -> float -> unit [@@js.set]
+
+val set_abr_max_with_real_bittrate : config -> bool -> unit [@@js.set]
+
+val set_max_starvation_delay : config -> int -> unit [@@js.set]
+
+val set_max_loading_delay : config -> int -> unit [@@js.set]
+
+val set_min_auto_bitrate : config -> int -> unit [@@js.set]
+
+val set_eme_enabled : config -> bool -> unit [@@js.set]
+
+val set_widevine_license_url : config -> string -> unit [@@js.set]
+
+val set_license_xhr_setup : config -> (xhr:xhr -> url:string -> unit) -> unit
+  [@@js.set]
+
+val set_drm_system_options : config -> drm_options -> unit [@@js.set]
+
+val set_request_media_key_system_access_func :
+  config -> RequestMediaKeySystemAccess.t -> unit
+  [@@js.set]
+
 val new_hls : ?config:config -> unit -> hls_item [@@js.new "Hls"]
+
+val get_config : hls_item -> config [@@js.get]
 
 [@@@js.stop]
 
